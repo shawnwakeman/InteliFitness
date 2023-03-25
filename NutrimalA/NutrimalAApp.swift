e798cb3d9c3ca1523a82a02745283fbcdc0ca4f5
Shawn Wakeman <wakemanshawn@gmail.com>
2023-03-25 14:37:03 -0500

    Initial Commit

---
+++ NutrimalA/NutrimalAApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutrimalAApp.swift
+//  NutrimalA
+//
+//  Created by Shawn Wakeman on 3/25/23.
+//
+
+import SwiftUI
+
+@main
+struct NutrimalAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

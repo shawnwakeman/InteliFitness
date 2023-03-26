9bec14e542e4dede288b710c989a1fe2ce01e09d
Shawn Wakeman <wakemanshawn@gmail.com>
2023-03-26 15:45:04 -0500

    Basic UI

--- NutrimalA/ContentView.swift
+++ NutrimalA/ContentView.swift
@@ -9,103 +9,185 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        
-
-        
         ScrollView(.vertical){
-            ZStack(){
-                
+            VStack(spacing: -20){
+                CoreLogModule()
                 RoundedRectangle(cornerRadius: 7)
-                    .stroke(Color.black, lineWidth: 2.5)
-                    .background(Color.clear)
-                
-                
-               
-                
-                VStack(alignment: .leading, spacing: 0){
-                    ZStack{
-                        RoundedRectangle(cornerRadius: 7)
-                            .stroke(Color.black, lineWidth: 2.5)
-                            .aspectRatio(7/1, contentMode: .fill)
-                        
-                        HStack{
-                            Text("Set").offset(CGSize(width: 5, height: 0))
-                                //.font(.custom(<#T##name: String##String#>, size: <#T##CGFloat#>))
-                            Spacer()
-                            Text("Previous").offset(CGSize(width: 10, height: 0))
-                            Spacer()
-                            Text("Lbs").offset(CGSize(width: 15, height: 0))
-                            Spacer()
-                            Text("Reps").offset(CGSize(width: 7, height: 0))
-                            Spacer()
-                        }.padding(.all)
-                    }
-
-                    
-                  
-                    
-                    row(setNumber: 1, prevouisSet: "135 lb x 61", setWeight: 23, SetReps: 121)
-                    Divider()
-                        .frame(height: 2.5)
-                        .overlay(.black)
-                    row(setNumber: 1, prevouisSet: "135 lb x 6", setWeight: 123, SetReps: 12)
-                    Divider()
-                        .frame(height: 2.5)
-                        .overlay(.black)
-
-                    row(setNumber: 1, prevouisSet: "135 lb x 6", setWeight: 123, SetReps: 12)
-                    Divider()
-                        .frame(height: 2.5)
-                        .overlay(.black)
-                    
-                    row(setNumber: 1, prevouisSet: "135 lb x 16", setWeight: 123, SetReps: 12)
-                    
-                }
-            } .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                    .padding(.all)
+                    .aspectRatio(7/1, contentMode: .fit)
+                    .foregroundColor(.clear)
+                    .strokeBorder(, antialiased: <#T##Bool#>)
+            }
+            
+            
         }
+        
+    }
+    
+    init() {
+        for famliyName in UIFont.familyNames {
+            print(famliyName)
+            for fontName in UIFont.fontNames(forFamilyName: famliyName){
+                print(fontName)
+            }
+                   
+        }
+    }
+    
+}
+
+struct CoreLogModule: View {
+    var body: some View{
+        ZStack(){
+            
+            RoundedRectangle(cornerRadius: 7)
+                .stroke(Color.black, lineWidth: 2.5)
+                .background(Color.clear)
+            
+            
+            
+            
+            VStack(alignment: .leading, spacing: 0){
+                Header()
+                ContentGrid()
+                
+            }
+            
+        }
+        .padding(.all)
+    }
+}
+
+struct TextHelvetica: View{
+    var content: String
+    var size: CGFloat
+    var body: some View
+    {
+        Text(content).font(.custom("HelveticaNeue-Medium", size: size)).multilineTextAlignment(.center)
     }
 }
 
 
-
-struct row: View{
+struct Row: View{
     var setNumber: Int
     var prevouisSet: String
     var setWeight: Int
     var SetReps: Int
     var body: some View{
-        let verticalBorderRect = Rectangle().frame(maxWidth: 2.5, maxHeight: .infinity)
+
         HStack(spacing: 0){
-            Text(String(setNumber))
+            TextHelvetica(content: String(setNumber), size: 24)
                 .padding()
-                .frame(width: 60, height: 40)
+                .frame(width: 55, height: 40)
                 .background(.clear)
-            verticalBorderRect
-            Text(prevouisSet)
-                .padding()
-                .frame(width: 140, height: 40)
+            Divider()
+                .frame(width: 2.5)
+                .overlay(.black)
+            TextHelvetica(content: prevouisSet, size: 24)
+                .frame(width: 145, height: 40)
                 .background(.clear)
-            verticalBorderRect
-            Text(String(setWeight))
+            Divider()
+                .frame(width: 2.5)
+                .overlay(.black)
+            TextHelvetica(content: String(setWeight), size: 24)
+                
+                .frame(width: 70, height: 40)
+                .background(.clear)
+            Divider()
+                .frame(width: 2.5)
+                .overlay(.black)
+
+            TextHelvetica(content: String(SetReps), size: 24)
                 .padding()
                 .frame(width: 75, height: 40)
                 .background(.clear)
-            verticalBorderRect
 
-            Text(String(SetReps))
-                .padding()
-                .frame(width: 75, height: 40)
-                .background(.clear)
-
-            verticalBorderRect
+            Divider()
+                .frame(width: 2.5)
+                .overlay(.black)
             Image(systemName: "checkmark")
                 .padding()
                 .frame(maxWidth: 40, maxHeight: 40)
                 .background(.clear)
+                
         }
         
     }
 }
+
+
+struct ContentGrid: View {
+    var body: some View{
+        Row(setNumber: 1, prevouisSet: "135 lb x 62", setWeight: 23, SetReps: 121)
+        Divider()
+            .frame(height: 2.5)
+            .overlay(.black)
+        Row(setNumber: 2, prevouisSet: "135 lb x 6", setWeight: 123, SetReps: 12)
+        Divider()
+            .frame(height: 2.5)
+            .overlay(.black)
+        
+        Row(setNumber: 3, prevouisSet: "135 lb x 62", setWeight: 123, SetReps: 12)
+        Divider()
+            .frame(height: 2.5)
+            .overlay(.black)
+        
+        Row(setNumber: 4, prevouisSet: "135 lb x 16", setWeight: 123, SetReps: 12)
+    }
+}
+
+
+
+struct Header: View {
+    var body: some View{
+        ZStack{
+            RoundedRectangle(cornerRadius: 7)
+                .stroke(Color.black, lineWidth: 2.5)
+                .aspectRatio(7/1, contentMode: .fill)
+            
+            HStack{
+                Spacer()
+                TextHelvetica(content: "Set", size: 27)
+                    .padding(.trailing)
+          
+                TextHelvetica(content: "Previous", size: 27)
+                    .padding(.trailing, 25)
+                
+                TextHelvetica(content: "Lbs", size: 27)
+                    .padding(.trailing)
+
+                TextHelvetica(content: "Reps", size: 27)
+                    .padding(.trailing, 25.0)
+                Spacer()
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 extension View {
     func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
@@ -126,28 +208,6 @@ struct RoundedCorner: Shape {
     
 }
 
-
-
-//
-//
-//    HStack {
-//        Text("This is a short string.")
-//            .padding()
-//            .frame(maxHeight: .infinity)
-//            .background(.red)
-//
-//        Text("This is a very long string with lots and lots of text that will definitely run across multiple lines because it's just so long.")
-//            .padding()
-//            .frame(maxHeight: .infinity)
-//            .background(.green)
-//    }
-//    .fixedSize(horizontal: false, vertical: true)
-//    .frame(maxHeight: 200)
-//
-//}
-//
-//
-//
 
 
 

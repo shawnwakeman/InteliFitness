d63bb0c0e784499a8f2e444a96eb84a10702e489
Shawn Wakeman <wakemanshawn@gmail.com>
2023-03-28 23:28:57 -0500

    Pushed to the verge of death

--- NutrimalA/WorkoutLogView.swift
+++ NutrimalA/WorkoutLogView.swift
@@ -7,15 +7,18 @@
 
 import SwiftUI
 var borderWeight: CGFloat = 1.7
-struct ContentView: View {
+struct WorkoutLogView: View {
     @State var progressValue: Float = 0.5
+
 
     var body: some View {
         
         ScrollView(.vertical){
             
             
-            MainHeaderForLog()
+            DropDownHeaderView()
+            
+            
             
             VStack(spacing: -20){
                 
@@ -33,19 +36,19 @@ struct ContentView: View {
         
     }
     
-    init() {
-        for famliyName in UIFont.familyNames {
-            print(famliyName)
-            for fontName in UIFont.fontNames(forFamilyName: famliyName){
-                print(fontName)
-            }
-                   
-        }
-    }
+//    init() {
+//        for famliyName in UIFont.familyNames {
+//            print(famliyName)
+//            for fontName in UIFont.fontNames(forFamilyName: famliyName){
+//                print(fontName)
+//            }
+//
+//        }
+//    }
     
 }
 
-struct MainHeaderForLog: View {
+struct DropDownHeaderView: View {
     var body: some View {
         ZStack{
             
@@ -58,6 +61,7 @@ struct MainHeaderForLog: View {
                     .padding(.vertical, -4.0)
                     .padding(.horizontal, -1)
                     .frame(height: 40)
+                    .shadow(color: .black, radius: 10, x: 0, y: 0)
                 RoundedRectangle(cornerRadius: 18)
                     .stroke(Color("BorderGray"), lineWidth: borderWeight)
                     .padding(.vertical, -4.0)
@@ -98,7 +102,7 @@ struct MainHeaderForLog: View {
                             .foregroundColor(Color("LinkBlue"))
                             .imageScale(.large)
                             .bold()
-                        TextHelvetica(content: "1:23", size: 23)
+                        TextHelvetica(content: "1:23", size: 20)
                             .foregroundColor(Color("WhiteFontOne"))
                     }
                     
@@ -128,9 +132,9 @@ struct MainHeaderForLog: View {
             }
             .padding(.all, 21.0)
             
-            TextHelvetica(content: "0:12", size: 25)
+            TextHelvetica(content: "0:12", size: 20)
                 .padding(.bottom, 17.0)
-                .foregroundColor(Color("WhiteFontOne"))
+                .foregroundColor(Color("GrayFontOne"))
         }.offset(x:0, y: -19)
     }
 }
@@ -147,9 +151,6 @@ struct ProgressBar: View {
                     .foregroundColor(Color("DBblack"))
                     .cornerRadius(45.0)
                     
-
-    
-               
                 
                 Rectangle().frame(width: min(CGFloat(0.5)*geometry.size.width, geometry.size.width), height: geometry.size.height)
                     .foregroundColor(Color(UIColor.systemBlue))
@@ -189,22 +190,19 @@ struct FullWidthButton: View{
 }
 struct LogModuleHeader: View{
     var body: some View{
+        
         HStack{
-            Button {
-                print("Button pressed")
-            }
             
+            Button {print("Button pressed")}
             label: {
-                TextHelvetica(content: "Back Squat", size: 32)
+                TextHelvetica(content: "Back Squat", size: 28)
                     .foregroundColor(Color("LinkBlue"))
             }
 
             Spacer()
             
             
-            Button {
-                print("Button pressed")
-            }
+            Button {print("Button pressed")}
             
             label: {
                 ZStack{
@@ -216,14 +214,28 @@ struct LogModuleHeader: View{
                 }
             }.frame(maxWidth: 100, maxHeight: 28)
             
-            
-            Image("dataIcon")
-                .resizable()
-                .frame(width: 39.4, height: 28)
-            
-            Image("meatBalls")
-                .resizable()
-                .frame(width: 39.4, height: 28)
+            ZStack{
+                Image("dataIcon")
+                    .resizable()
+                    .frame(width: 39.4, height: 28)
+                
+                RoundedRectangle(cornerRadius: 3)
+                    .stroke(Color("BorderGray"), lineWidth: borderWeight)
+                    .frame(width: 39.4, height: 28)
+            }
+
+            ZStack{
+                
+                Image("meatBalls")
+                    .resizable()
+                    .frame(width: 39.4, height: 28)
+                RoundedRectangle(cornerRadius: 3)
+                
+                    .stroke(Color("BorderGray"), lineWidth: borderWeight)
+                    .frame(width: 39.4, height: 28)
+            }
+
+                
             
         }
         .padding(/*@START_MENU_TOKEN@*/[.top, .leading, .trailing]/*@END_MENU_TOKEN@*/)
@@ -264,15 +276,18 @@ struct TextHelvetica: View{
 }
 
 
-struct Row: View{
+struct WorkoutSetRowView: View{
     var setNumber: Int
-    var prevouisSet: String
+    var previousSet: String
     var setWeight: Int
     var SetReps: Int
+    @State private var givenName: String = ""
+    @State private var familyName: String = ""
+
     var body: some View{
 
         HStack(spacing: 0){
-            TextHelvetica(content: String(setNumber), size: 24)
+            TextHelvetica(content: String(setNumber), size: 21)
                 .padding()
                 .frame(width: 55, height: 40)
                 .foregroundColor(Color("LinkBlue"))
@@ -281,29 +296,91 @@ struct Row: View{
                 .frame(width: borderWeight)
                 .overlay(Color("BorderGray"))
            
-            TextHelvetica(content: prevouisSet, size: 24)
+            TextHelvetica(content: previousSet, size: 21)
                 .frame(width: 145, height: 40)
                 .foregroundColor(Color("GrayFontOne"))
                 .background(.clear)
             Divider()
                 .frame(width: borderWeight)
                 .overlay(Color("BorderGray"))
-            TextHelvetica(content: String(setWeight), size: 24)
-                .frame(width: 70, height: 40.0)
-                .foregroundColor(Color("WhiteFontOne"))
-                .background(Color("DDB"))
+            
+            
+   
+
+
+//            ZStack {
+//
+//                if familyName.isEmpty {
+//                    Text("13")
+//                        .foregroundColor(Color("GrayFontOne"))
+//                        .font(.custom("HelveticaNeue-Medium", size: 21))
+//
+//                }
+//
+//                TextField("", text: $familyName)
+//                    .font(.custom("HelveticaNeue-Medium", size: 21))
+//                    .foregroundColor(Color("WhiteFontOne"))
+//            }
+//            .frame(width: 70, height: 40)
+//            .background(Color("DDB"))
+//
+////
+//
+
+//                if givenName.isEmpty {
+//                    Text("")
+//                        .foregroundColor(Color("GrayFontOne"))
+//                        .font(.custom("HelveticaNeue-Medium", size: 21))
+//
+//                }
+            
+//
+            ZStack{
                 
+                TextField("123", text: $givenName)
+                    .font(.custom("HelveticaNeue-Medium", size: 21))
+                    
+                    .foregroundColor(Color(red: 0.2713462753, green: 0.2713462753, blue: 0.2713462753))
+                    .blendMode(.darken)
+                    
+                    .multilineTextAlignment(.center)
+                    .frame(width: 70, height: 40)
+                    .background(.clear)
+                    .colorInvert()
+                    .accentColor(.orange)
+                 
+                    
+            }.background(Color("DDB"))
+                
+
+//
+//
+//
+            
             Divider()
                 .frame(width: borderWeight)
                 .overlay(Color("BorderGray"))
+            
             HStack{
                 
 
                
-                TextHelvetica(content: String(SetReps), size: 24)
-                   
-                    .frame(maxWidth: 30)
-                    .foregroundColor(Color("WhiteFontOne"))
+                ZStack{
+                    
+                    TextField("12", text: $givenName)
+                        .font(.custom("HelveticaNeue-Medium", size: 21))
+                        
+                        .foregroundColor(Color(red: 0.2713462753, green: 0.2713462753, blue: 0.2713462753))
+                        .blendMode(.darken)
+                        
+                        .multilineTextAlignment(.center)
+                        .frame(width: 25, height: 40)
+                        .background(.clear)
+                        .colorInvert()
+                        .accentColor(.orange)
+                     
+                        
+                }.background(Color("DDB"))
                     
                     
                 ZStack{
@@ -312,17 +389,17 @@ struct Row: View{
                         Capsule()
                             .padding(.vertical, 9.0)
                             .foregroundColor(Color("MainGray"))
-                            .frame(width: 35)
+                            .frame(width: 30)
                         
                         Capsule()
                             .strokeBorder(Color("BorderGray"), lineWidth: borderWeight)
                             .padding(.vertical, 9.0)
                             
                             
-                            .frame(width: 35)
+                            .frame(width: 30)
                     }
                     
-                    TextHelvetica(content: "10", size: 14)
+                    TextHelvetica(content: "10", size: 13)
                         .foregroundColor(Color.white)
                         
                 }
@@ -349,27 +426,41 @@ struct Row: View{
     }
 }
 
+struct SuperTextField: View {
+    
+    var placeholder: Text
+    @Binding var text: String
+    var editingChanged: (Bool)->() = { _ in }
+    var commit: ()->() = { }
+    
+    var body: some View {
+        ZStack(alignment: .leading) {
+            if text.isEmpty { placeholder }
+            TextField("", text: $text, onEditingChanged: editingChanged, onCommit: commit)
+        }
+    }
+}
 
 struct ContentGrid: View {
     var body: some View{
-        Row(setNumber: 1, prevouisSet: "135 lb x 62", setWeight: 23, SetReps: 10)
+        WorkoutSetRowView(setNumber: 1, previousSet: "135 lb x 62", setWeight: 23, SetReps: 10)
         Divider()
             
             .frame(height: borderWeight)
             .overlay(Color("BorderGray"))
             
-        Row(setNumber: 2, prevouisSet: "135 lb x 6", setWeight: 123, SetReps: 6)
+        WorkoutSetRowView(setNumber: 2, previousSet: "135 lb x 6", setWeight: 123, SetReps: 6)
         Divider()
             .frame(height: borderWeight)
             .overlay(Color("BorderGray"))
 
         
-        Row(setNumber: 3, prevouisSet: "135 lb x 62", setWeight: 123, SetReps: 12)
+        WorkoutSetRowView(setNumber: 3, previousSet: "135 lb x 62", setWeight: 123, SetReps: 12)
         Divider()
             .frame(height: borderWeight)
             .overlay(Color("BorderGray"))
         
-        Row(setNumber: 4, prevouisSet: "135 lb x 16", setWeight: 123, SetReps: 12)
+        WorkoutSetRowView(setNumber: 4, previousSet: "135 lb x 16", setWeight: 123, SetReps: 12)
     }
 }
 
@@ -385,21 +476,21 @@ struct Header: View {
             VStack{
                 HStack{
                     Spacer()
-                    TextHelvetica(content: "Set", size: 27)
+                    TextHelvetica(content: "Set", size: 24)
                         .foregroundColor(Color("WhiteFontOne"))
                         .offset(x: -2)
                     Spacer()
               
-                    TextHelvetica(content: "Previous", size: 27)
+                    TextHelvetica(content: "Previous", size: 24)
                         .foregroundColor(Color("WhiteFontOne"))
                     Spacer()
                     
-                    TextHelvetica(content: "Lbs", size: 27)
+                    TextHelvetica(content: "Lbs", size: 24)
                         .foregroundColor(Color("WhiteFontOne"))
                         .offset(x:5)
                     Spacer()
 
-                    TextHelvetica(content: "Reps", size: 27)
+                    TextHelvetica(content: "Reps", size: 24)
                         .padding(.trailing)
                         .foregroundColor(Color("WhiteFontOne"))
                 
@@ -476,6 +567,6 @@ struct RoundedCorner: Shape {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        WorkoutLogView()
     }
 }
